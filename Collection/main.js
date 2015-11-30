0eb0c22f5c605bcdb66c19a0bbc91735e71cc015
hannahebernard <hebernard@massart.edu>
2015-11-30 09:33:19 -0500

    edit

--- Collection/main.js
+++ Collection/main.js
@@ -1,3 +1,14 @@
+
+function callback(data) {
+    console.log(data);  
+  }
+
+  Tabletop.init( { key: "https://docs.google.com/spreadsheets/d/1_EmMnzRcE7C9hv4wN_k8Izq46zYYENvqWAk7k1I-j3s/edit?usp=sharing",
+                   callback: callback,
+                   simpleSheet: true } )
+
+
+
 $(".book1").mouseenter(function(){
   $(".bookhover").show();
 });
