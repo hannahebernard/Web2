b5256c7ddf969c7563274a5a71d86efa7dfa4cd3
hannahebernard <hebernard@massart.edu>
2015-12-13 14:07:15 -0500

    images

--- Collection/main.js
+++ Collection/main.js
@@ -56,7 +56,6 @@ function showModal(item) {
    var $author = $("<p class='info'>").text(item.Author);
    var $location = $("<p class='info'>").text(item.Location);
    var $date = $("<p class='info'>").text(item.Published);
-   var $started = $("<p class='info'>").text(item.Started);
 
    var url = item.Large
 
@@ -64,7 +63,7 @@ function showModal(item) {
 
   
    $(".modal").append($infobox).append($thumbnail).append($title)
-  $(".infobox").append($started).append($author).append($location).append($date);
+  $(".infobox").append($author).append($date).append($location);
    $(".overlay").show();
    $(".modal").show();
 
@@ -87,10 +86,31 @@ function callback(data) {
 
  addBooks(data);
 
-  var myCollection = $(".item").collection({
+  /* var $container = $('.collection');
+  $container.packery({
+    itemSelector: '.book',
+    gutter: 10,
+  });*/
+
+  myCollection = $(".book").collection({
     filters: { 
-      "location-facet": "[data-location]"
-    }
+      "location-facet": "[data-location]",
+      "genre-facet": "[data-genre]",
+      "started-facet": "[data-started]",
+      "date-facet": "[data-date]",
+    },
+
+     /* update: function() { 
+      $container.packery(); 
+      setTimeout(function() { $container.packery()},600);
+    },
+    ordered: function() {
+      $container.packery('reloadItems');
+      console.log("Whoa");
+    },
+    hide: function($elem) { $elem.hide(); },
+    show: function($elem) { $elem.fadeIn(500); },*/
+
   });
 
 
@@ -98,7 +118,87 @@ function callback(data) {
 
       var location = $(this).data("location");
 
+     var text;
+      if(location) {
+        text = $(this).text() ;
+      } else {
+        text = "Location";
+      }
+
+      $("#locationfilter").text(text)
+
+
       myCollection.filtered("location-facet",location);
+  });
+
+      $(".genre").on("click",function(e) {
+
+      var genre = $(this).data("genre");
+
+         var text;
+      if(genre) {
+        text = $(this).text() ;
+      } else {
+        text = "Genre";
+      }
+
+      $("#genrefilter").text(text)
+
+      myCollection.filtered("genre-facet",genre);
+  });
+
+
+
+        $(".started").on("click",function(e) {
+
+      var started = $(this).data("started");
+
+       var text;
+      if(started) {
+        text = $(this).text() ;
+      } else {
+        text = "Started";
+      }
+
+      $("#startedfilter").text(text)
+      myCollection.filtered("started-facet",started);
+  });
+
+
+
+
+        $(".date").on("click",function(e) {
+
+      var date = $(this).data("date");
+      var text;
+      if(date) {
+        text = $(this).text() ;
+      } else {
+        text = "Date Published";
+      }
+
+      $("#publishedfilter").text(text)
+      myCollection.filtered("date-facet",date);
+
+
+  });
+
+
+
+     $(".all").on("click",function(e) {
+      myCollection.filtered("location-facet","");
+      myCollection.filtered("genre-facet","");
+      myCollection.filtered("started-facet","");
+      myCollection.filtered("date-facet","");
+
+       $("#publishedfilter").text("Date Published")
+       $("#genrefilter").text("Genre")
+       $("#locationfilter").text("Location")
+       $("#startedfilter").text("Started")
+  });
+
+  $(".searchbar").on("change keyup",function(e) {
+      myCollection.filtered("title", $(this).val());
   });
 
 
@@ -128,6 +228,9 @@ function addBooks(booksData) {
   var $overlay = $(".overlay")
 
   $book.attr("data-location",item.Location);
+  $book.attr("data-genre",item.Genre);
+  $book.attr("data-started",item.Started);
+  $book.attr("data-date",item.Date);
 
   $book.append($thumbnail)
   
@@ -142,76 +245,6 @@ function addBooks(booksData) {
   });
 
   }
-
-
-/*function showModal(item){
-
-
-var url = item.Photo
- var title = video.snippet.title;
-  var videoId = video.id.videoId;
-
-  var $overlay = (".overlay")
-
-
-  
-	$(".modal").empty();
-   	$(".modal").append($thumbnail).append($title)
-  	$(".modal").show();
-}*/
-
-
-
-
-
-  /*$container.packery({
-    itemSelector: '.item',
-    gutter: 10,
-  });
-
-  var myCollection = $(".item").collection({
-    filters: { 
-      "title": "h2",
-      "category": "[data-category]"
-    },
-
-    update: function() { 
-      $container.packery(); 
-      setTimeout(function() { $container.packery()},600);
-    },
-    ordered: function() {
-      $container.packery('reloadItems');
-      console.log("Whoa");
-    },
-    hide: function($elem) { $elem.hide(); },
-    show: function($elem) { $elem.fadeIn(500); },
-  });
-
-  $(".category").on("click",function(e) {
-      var category = $(this).data("category");
-      $("#filter-bar button.category").removeClass("selected");
-      $(this).addClass("selected");
-      myCollection.filtered("category",category);
-  });
-
-
-  $(".order").on("click",function(e) {
-      var order = $(this).data("order");
-      $("#filter-bar button.category").removeClass("selected");
-      $(this).addClass("selected");
-      myCollection.ordered(order);
-  });
-
-
-
-  $("#search").on("change keyup",function(e) {
-      myCollection.filtered("title", $(this).val());
-  });
-
-  $(".overlay").on("click",function(e) {
-      hideModal();
-  });
-*/
 
 
   Tabletop.init( { key: "https://docs.google.com/spreadsheets/d/1_EmMnzRcE7C9hv4wN_k8Izq46zYYENvqWAk7k1I-j3s/pubhtml?gid=0&single=true",
